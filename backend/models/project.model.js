407311c8afa67bdff69f0083ec90e43ee6bea990
Kunal Bhatnagar <kbhatnagar17.kb@gmail.com>
2025-02-13 13:42:33 -0500

    controllers and routes updated

--- backend/models/project.model.js
+++ backend/models/project.model.js
@@ -1,21 +1,30 @@
 import mongoose from 'mongoose';
 
+
 const projectSchema = new mongoose.Schema({
     name: {
         type: String,
         lowercase: true,
         required: true,
         trim: true,
-        unique: true,
+        unique: [ true, 'Project name must be unique' ],
     },
-    user: [
+
+    users: [
         {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'user'
         }
-    ]
+    ],
+    fileTree: {
+        type: Object,
+        default: {}
+    },
+
 })
 
+
 const Project = mongoose.model('project', projectSchema)
+
 
 export default Project;
